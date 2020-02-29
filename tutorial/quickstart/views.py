be1390f131000211da20311a9124d74fefd45bd8
mohammad-fouladgar <fouladgar.dev@gmail.com>
2020-02-29 12:20:56 +0330

    Fix gitignore

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 from .serializers import UserSerializer, GroupSerializer
 
 
-
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
